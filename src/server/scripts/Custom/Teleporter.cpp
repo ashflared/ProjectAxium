3c9a1e511d4d448bff49b5042572ae15f201c7de
Ashley <saeshyls@gmail.com>
2018-08-13 12:50:31 +0100

    Remove Old Mall and Zone options from the Teleporter NPC

--- src/server/scripts/Custom/Teleporter.cpp
+++ src/server/scripts/Custom/Teleporter.cpp
@@ -16,9 +16,8 @@ class Teleporter : public CreatureScript
         {
             player->PlayerTalkClass->ClearMenus();
 
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "|TInterface\\icons\\achievement_boss_zuramat:30|t Mall", GOSSIP_SENDER_MAIN, TELEPORT_MALL);
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "|TInterface\\icons\\achievement_zone_howlingfjord_03:30|t Dueling Zone", GOSSIP_SENDER_MAIN, TELEPORT_DUEL);
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "|TInterface\\icons\\inv_misc_armorkit_14:30|t Gurubashi Arena", GOSSIP_SENDER_MAIN, TELEPORT_GURUBASHI);
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Mall", GOSSIP_SENDER_MAIN, TELEPORT_MALL);
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Dueling Zone", GOSSIP_SENDER_MAIN, TELEPORT_DUEL);
             player->SEND_GOSSIP_MENU(1, creature->GetGUID());
             return true;
         }
@@ -29,23 +28,10 @@ class Teleporter : public CreatureScript
             {
                 case TELEPORT_MALL:
                 {
-                    player->RemoveAura(SPELL_GURUBASHI_BANISH);
-                    player->TeleportTo(558, 72.5f, -163.0f, 15.5f, 3.1f);
                     break;
                 }
                 case TELEPORT_DUEL:
                 {
-                    player->RemoveAura(SPELL_GURUBASHI_BANISH);
-                    player->TeleportTo(571, 524.0f, -3237.0f, 27.5f, 3.3f);
-                    break;
-                }
-                case TELEPORT_GURUBASHI:
-                {
-                    player->Dismount();
-                    player->RemoveAurasByType(SPELL_AURA_MOD_SHAPESHIFT);
-                    player->RemoveAllControlled();
-                    player->AddAura(SPELL_GURUBASHI_BANISH, player);
-                    player->TeleportTo(0, -13315.0f, 80.0f, 22.15f, 1.0f);
                     break;
                 }
             }
